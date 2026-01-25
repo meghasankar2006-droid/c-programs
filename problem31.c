0b7f87caaa24fb6e59c630516f694738b178d974
Meghasri <meghasankar2006@gmail.com>
2026-01-25 12:57:18 +0530

    Add files via upload

---
+++ problem31.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() {
+int num=10;
+num+=2;
+printf("%d\n",num);
+int num2=5;
+num2+=2;
+printf("%d",num2);
+
+    return 0;
+}
