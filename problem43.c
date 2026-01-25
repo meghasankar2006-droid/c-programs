4e6112b17b0615a411f8f8a2bda937767f3960af
Meghasri <meghasankar2006@gmail.com>
2026-01-25 14:26:42 +0530

    Update problem43.c

--- problem43.c
+++ problem43.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 
 int main() {
-    int num1=10;
-    num1+=5;
-    printf("%d",num1);
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    printf("%d",num1+=num2);
 
     return 0;
+
 }
