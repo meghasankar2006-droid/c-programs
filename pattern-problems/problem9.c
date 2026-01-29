20ae971ae668859b816e6dd229b665d3e253f819
Meghasri <meghasankar2006@gmail.com>
2026-01-29 15:01:31 +0530

    Add files via upload

---
+++ pattern-problems/problem9.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int k=n;
+    for(int i=1;i<=n;i++){
+        for(int s=1;s<=n-i;s++){
+            printf(" ");
+        }
+        for(int j=1;j<=2*i-1;j++){
+        printf("*");
+        }
+        printf("\n");
+    }
+    for(int i=0;i<n;i++){
+        for(int s=n;s>n-i;s--){
+            printf(" ");
+        }
+        for(int j=1;j<=2*k-1;j++){
+        printf("*");
+        }
+        k--;
+        printf("\n");
+    }
+        
+        return 0;
+    }
