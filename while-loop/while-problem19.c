bcb1819a886fbdf8734a7be43ace1c00fbef76fd
Meghasri <meghasankar2006@gmail.com>
2026-02-03 13:12:01 +0530

    Update while-problem19.c

--- while-loop/while-problem19.c
+++ while-loop/while-problem19.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 int main(){
-    int mcap,n,hour;
-    scanf("%d %d",&mcap,&n);
-    int sum=0,count=0,rem=0;
-        while(n>0){
-            scanf("%d",&hour);
-            
-            if(mcap>=hour){
-                count=count+hour;
-                mcap=mcap-hour;
-            }
-            else{
-                rem=rem+hour;
-            }
+    int tot,n,hour;
+    scanf("%d %d",&tot,&n);
+    int sum=0;
+    while(n>0){
+        scanf("%d",&hour);
+        sum=sum+hour;
         n--;
-        }
-        printf("Treated Patients:%d\n",count);
+    }
+    if(sum>tot){
+        int rem=sum-tot;
+        printf("Treated Patients:%d",tot);
         printf("Rejected Patients:%d",rem);
+    }
+    if(sum<tot){
+        printf("Treated Patients:%d\n",tot);
+        printf("Rejected Patients:%d",0);
+    
+    }
     }
