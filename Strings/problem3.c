08bd6abae408c2fc049f2d59ae4b3b66b53ef261
Meghasri <meghasankar2006@gmail.com>
2026-02-09 22:22:29 +0530

    Add files via upload

---
+++ Strings/problem3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    char str[200];
+    scanf("%s",str);
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]=='@'){
+            break;
+        }
+        printf("%c",str[i]);
+    }
+}
---
+++ Strings/problem4.c
@@ -0,0 +1,12 @@
+//Reverse string without strrev().
+
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[200];
+    scanf("%s",str);
+    int len=strlen(str);
+    for(int i=len-1;i>=0;i--){
+        printf("%c",str[i]);
+    }
+}
