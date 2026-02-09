d5f18d811ef63f5310e6b0958bc77ea4deff2417
Meghasri <meghasankar2006@gmail.com>
2026-02-09 23:43:15 +0530

    Add files via upload

---
+++ Strings/problem7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    char str[200];
+    scanf("%s",str);
+    int count=0;
+    for(int i=0;str[i]!=0;i++){
+        if((str[i]>'a'&& str[i]>'z')||str[i]>'A'&& str[i]>'Z')
+        if (str[i]!='a'&& str[i]!='e'&& str[i]!='i'&& str[i]!='o'&& str[i]!='u'&& str[i]!='A'&& str[i]!='O'&& str[i]!='I'&& str[i]!='E'&& str[i]!='U'){
+            count++;
+        }
+    }
+    printf("%d",count);
+}
