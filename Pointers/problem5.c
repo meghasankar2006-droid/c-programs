b1e3c9dab644f94005e619e71c0b0b67c7314255
Meghasri <meghasankar2006@gmail.com>
2026-02-13 11:33:10 +0530

    Add files via upload

---
+++ Pointers/problem5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int sum_val(int arr[],int n){
+    int *p=arr;
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum=sum+*(p+i);
+    }
+    printf("%d",sum);
+
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    sum_val(arr,n);
+
+}
