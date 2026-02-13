c21ff06eb89f58eedd6a332ce63612688b467efc
Meghasri <meghasankar2006@gmail.com>
2026-02-13 11:36:52 +0530

    Add files via upload

---
+++ Pointers/problem10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int count_str(char *c,int *num){
+    int count=1;
+    for(int i=0;i<*num;i++){
+        if(*(c+i)==' '){
+            count++;
+        }
+    }
+    printf("%d",count);
+}
+int main(){
+    char str[200];
+    fgets(str,sizeof(str),stdin);
+    int n=strlen(str);
+    count_str(str,&n);
+}
