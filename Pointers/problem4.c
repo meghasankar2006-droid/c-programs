3bbc9a9e4e8a9763a8841ab787e99fb7a56a708e
Meghasri <meghasankar2006@gmail.com>
2026-02-13 11:32:43 +0530

    Add files via upload

---
+++ Pointers/problem4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+char copy(char *p ,char *c){
+    int i;
+    for( i=0;*(p+i)!=0;i++){
+        *(c+i)=*(p+i);
+    }
+    *(c+i)='\0';
+    
+}
+int main(){
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    char str1[100];
+    copy(str,str1);
+    printf("%s",str1);
+}
