e5905221bd59ad5610880ecabcf597440865e1fb
Meghasri <meghasankar2006@gmail.com>
2026-02-21 11:57:12 +0530

    Update problem1.c

--- Pointers/problem1.c
+++ Pointers/problem1.c
@@ -4,13 +4,10 @@ void swap(int*x ,int *y){
     *x=*y;
     *y=temp;
     printf("After Swapping %d %d",*x,*y);
-
-
 }
 int main(){
     int a,b;
     scanf("%d %d",&a,&b);
     printf("Before Swapping %d %d\n",a,b);
     swap(&a,&b);
-
 }
